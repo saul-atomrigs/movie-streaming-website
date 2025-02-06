a2976725991ff65375252818603e0155f3ad4b59
Saul Lee <solleedata@gmail.com>
2025-02-06 11:44:01 +0900

    feat: remove server-side session handling from index and profile pages

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -2,25 +2,6 @@ import Billboard from '@/src/components/billboard';
 import useMovieList from '@/src/components/movies/hooks/useMovieList';
 import Movie from '@/src/components/movies/Movie';
 import NavigationBar from '@/src/components/navigation/NavigationBar';
-import { NextPageContext } from 'next';
-import { getSession } from 'next-auth/react';
-
-// export async function getServerSideProps(context: NextPageContext) {
-//   const session = await getSession(context);
-
-//   if (!session) {
-//     return {
-//       redirect: {
-//         destination: '/auth',
-//         permanent: false,
-//       },
-//     };
-//   }
-
-//   return {
-//     props: {},
-//   };
-// }
 
 export default function Home() {
   const { data: movies = [] } = useMovieList();
