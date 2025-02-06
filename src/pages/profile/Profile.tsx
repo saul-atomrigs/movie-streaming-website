bdc6788400a6b71216e9c7fda8e74752d171c18a
Saul Lee <solleedata@gmail.com>
2025-02-06 16:27:28 +0900

    feat: restructure authentication and profile components with new directory structure

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -2,7 +2,7 @@ import ProfileName from './components/ProfileName';
 import ProfileImage from './components/ProfileImage';
 import ProfileContainer from './components/ProfileContainer';
 import Header from './components/Header';
-import CenterLayout from '@/src/components/CenterLayout';
+import CenterLayout from '@/components/CenterLayout';
 
 export default function Profile() {
   return (
